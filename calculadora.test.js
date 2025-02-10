bb322e70d6722dce8eabbd91aa26886b124ce33d
Adam_SM93 <lsanmar1207b@g.educaand.es>
2025-02-10 11:14:34 +0100

    index

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,4 +1,4 @@
-const { sumar, restar, multiplicar, dividir, raizCuadrada } = require('./calculadora');
+const { sumar, restar, multiplicar, dividir } = require('./calculadora');
 
 test('Suma 2 + 3 y devuelve 5', () => {
     expect(sumar(2, 3)).toBe(5);
@@ -14,12 +14,4 @@ test('Multiplica 2 * 3 y devuelve 6', () => {
 
 test('Divide 6 / 2 y devuelve 3', () => {
     expect(dividir(6, 2)).toBe(3);
-});
-
-test('Raíz cuadrada de 9 y devuelve 3', () => {
-    expect(raizCuadrada(9)).toBe(3);
-});
-
-test('Raíz cuadrada de 16 y devuelve 4', () => {
-    expect(raizCuadrada(16)).toBe(4);
 });

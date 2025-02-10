d66a1e43c54a07b38dca9ce2d9bd91ad877ee6aa
Adam_SM93 <lsanmar1207b@g.educaand.es>
2025-02-10 11:18:28 +0100

    index3

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,21 @@
-function suma(a, b) {
+function sumar(a, b) {
   return a + b;
 }
-function resta(a, b) {
+
+function restar(a, b) {
   return a - b;
 }
+
 function multiplicar(a, b) {
   return a * b;
 }
+
 function dividir(a, b) {
-  if (b === 0) {
-    throw new Error("No se puede dividir por cero");
-  }
   return a / b;
 }
-// // Hacer las funciones accesibles en el navegador
-// window.suma = suma;
-// window.resta = resta;
-// window.multiplicar = multiplicar;
-// window.dividir = dividir;
+
+function raizCuadrada(a) {
+  return Math.sqrt(a);
+}
+
+module.exports = { sumar, restar, multiplicar, dividir, raizCuadrada };
